57639a515985b0f3f8e8f742cc1835b83f7fd400
Chance Hollis <chancehollis@gmail.com>
2016-11-03 17:29:50 -0600

    Added functionality to Home controller

--- WebBaseSoftwareProject/Controllers/HomeController.cs
+++ WebBaseSoftwareProject/Controllers/HomeController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBaseSoftwareProject.Models;
 
 namespace WebBaseSoftwareProject.Controllers
 {
     public class HomeController : Controller
     {
+        User testUser = new User() { UserName = "Test", Password = "password" };
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(User u)
         {
-            return View();
+            return View(u);
         }
 
         public ActionResult LogIn()
@@ -19,11 +21,20 @@ namespace WebBaseSoftwareProject.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult Test()
-        //{
-        //    return View();
-        //}
-        //hello
+        [HttpPost]
+        public ActionResult LogIn(User u)
+        {
+            ViewResult result = null;
+            if (ModelState.IsValid)
+            {
+                result = View("Index", u);
+            }
+            else
+            {
+                ViewBag.error = "Invalid user";
+                result = View();
+            }
+            return result;
+        }
     }
 }
