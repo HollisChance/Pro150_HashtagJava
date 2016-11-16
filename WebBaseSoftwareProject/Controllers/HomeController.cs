bed15bff25d412c43ff608a52d53cf351a756898
BrandonCoats <bgcoats1313@gmail.com>
2016-11-15 17:36:30 -0700

    The changes to upload for getting an image

--- WebBaseSoftwareProject/Controllers/HomeController.cs
+++ WebBaseSoftwareProject/Controllers/HomeController.cs
@@ -91,6 +91,27 @@ namespace WebBaseSoftwareProject.Controllers
             }
             return result;
         }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+                try
+                {
+                    string path = Path.Combine(Server.MapPath("~/Images"),
+                                               Path.GetFileName(file.FileName));
+                    file.SaveAs(path);
+                    ViewBag.Message = "File uploaded successfully";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                }
+            else
+            {
+                ViewBag.Message = "You have not specified a file.";
+            }
+            return View();
+        }
     }
    
 }
